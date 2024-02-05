b62fe9f67c656dc3a0f7ed78699c5b20c4e0e61b
Dillon <u1348996@utah.edu>
2024-02-05 13:26:49 -0700

    Commit - 020524

--- lab4/my_proj/my_app/views.py
+++ lab4/my_proj/my_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, redirect
 class HomePageView(View):
     def get(self, request):
      
-        return render(request, 'my_app/index.html',{'hi':'hello world!'})
+        return render(request, 'my_app/index.html',{'hi':'hello universe!'})
