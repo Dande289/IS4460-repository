3f2b1803758da6e61765cab5a00ae56ab513be13
Dillon <u1348996@utah.edu>
2024-02-06 19:06:52 -0700

    Commit - 020624

--- lab4/my_proj/my_app/views.py
+++ lab4/my_proj/my_app/views.py
@@ -1,7 +1,24 @@
 from django.views import View
 from django.shortcuts import render, redirect
 
+
+def title_name(the_name: str):
+    return the_name.title()
+
 class HomePageView(View):
     def get(self, request):
-     
-        return render(request, 'my_app/index.html',{'hi':'hello universe!'})
+
+        my_name = "dILLON"
+
+        new_name = title_name(my_name)
+
+        names = ['Dillon', 'Robert','Brayden']
+
+        the_context = {'hi':'hello universe!',
+                       'name': new_name,
+                       'orig_name' : my_name,
+                       'names' : names
+                       }
+
+        return render(request, 'my_app/index.html',
+                      the_context)
