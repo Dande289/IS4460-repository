eba12d86f79c29162074113b9508bff8736c3caf
Dillon <u1348996@utah.edu>
2024-02-08 16:42:56 -0700

    Commit - 020824

--- lab4/my_proj/my_app/my_objects.py
+++ lab4/my_proj/my_app/my_objects.py
@@ -1,6 +1,11 @@
 class vehicle():
-    def __init__(self):
+    def __init__(self, make, model, year, color, sound):
         self.type = 'generic vehicle'
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+        self.sound = sound
 
 
 class car (vehicle):
@@ -8,10 +13,15 @@ class car (vehicle):
         self.color = color
         self._sound = sound
         self.wheels = 4
+    
+    def honk(self):
+        return self._sound
 
-class motorcycle(vehicle):
-    def __init__(self, color, sound):
+class motorcycle (vehicle):
+    def __init__(self, color, sound, make, year):
         self.color= color
-        self_sound = sound
+        self.sound = sound
+        self.make = make
+        self.year = year
         self.wheels = 2      
 
--- lab4/my_proj/my_app/views.py
+++ lab4/my_proj/my_app/views.py
@@ -16,10 +16,11 @@ class HomePageView(View):
 
         fixed_names = my_functions.fix_names_list(names)
 
-        car1 = my_objects.car('green', 'honk honk')
+        car1 = my_objects.car(color = 'green', sound = 'honk honk')
         
-        car2 = my_objects.car('blue', 'beep beep')
+        car2 = my_objects.car(color = 'blue', sound = 'beep beep')
 
+        motorcycle1 = my_objects.motorcycle(color = '', sound = '', make = 'Husqvarna', year = '2018')
 
         the_context = {'hi':'hello universe!',
                        'name': new_name,
@@ -28,6 +29,7 @@ class HomePageView(View):
                        'fixed_names' : fixed_names,
                        'car1' : car1,
                        'car2' : car2,
+                       'motorcycle1' : motorcycle1,
                        }
 
         return render(request, 'my_app/index.html',
