6834143f6d21e9daaf2b12154a62551bd6afddbe
Dillon <u1348996@utah.edu>
2024-03-18 14:23:26 -0600

    Test commit

---
+++ API/api/customer/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics
+from .models import Customer
+from django.shortcuts import render
+from .serializers import CustomerSerializer
+
+class CustomerListCreateView(generics.ListCreateAPIView):
+
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+# Create your views here.
